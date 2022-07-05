4c3c56e5e4b440aaeb34011dc77938c7a6676712
Renato Shigueo Fujimoto <98925481+Renatofujimoto@users.noreply.github.com>
2022-07-04 22:01:55 -0300

    Update mapThis.js

--- JavaScript/maps-filter-reduce/maps/mapThis.js
+++ JavaScript/maps-filter-reduce/maps/mapThis.js
@@ -16,3 +16,15 @@ const nums = [1, 2]
 
 console.log('this => orange', mapWithThis(nums, orange))
 console.log('this => apple', mapWithThis(nums, apple))
+
+// segunda resolução
+
+function mapArray() {
+  const array = [1, 2, 3, 4, 5]
+
+  return array.map(function (item) {
+    return item * this.price
+  }, apple)
+}
+
+console.log(mapArray())
