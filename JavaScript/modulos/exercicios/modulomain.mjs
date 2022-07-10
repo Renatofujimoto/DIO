e9d817a59cc2f042c6f5966538c4865925a957c4
Renato Shigueo Fujimoto <98925481+Renatofujimoto@users.noreply.github.com>
2022-07-09 23:59:29 -0300

    Add files via upload

---
+++ JavaScript/modulos/exercicios/modulomain.mjs
@@ -0,0 +1,4 @@
+import { showCity, showAge, showHooby } from './modulos.mjs'
+console.log(showAge('Renato', 28))
+console.log(showCity('Renato', 'São Paulo'))
+console.log(showHooby('Renato', 'Ler livros'))
---
+++ JavaScript/modulos/exercicios/modulos.mjs
@@ -0,0 +1,11 @@
+export function showAge(nome, idade) {
+  return `A idade de ${nome} e ${idade}.`
+}
+
+export function showCity(nome, cidade) {
+  return `A cidade de ${nome} e ${cidade}!`
+}
+
+export function showHooby(nome, hobby) {
+  return `O hobby de ${nome} e ${hobby}!`
+}
