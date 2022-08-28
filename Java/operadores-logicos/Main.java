dac7f86675f557f920d13579aefad7e673fee724
Renato Shigueo Fujimoto <98925481+Renatofujimoto@users.noreply.github.com>
2022-08-28 20:21:39 -0300

    Add files via upload

---
+++ Java/operadores-logicos/Main.java
@@ -0,0 +1,23 @@
+package operadoresLogicos;
+
+public class Main {
+    public static void main(String[] args){
+
+    double salarioMensal = 11893.58;
+    double mediaSalario = 10500d;
+
+    int quantidadeDependentes = 4;
+    int mediaDependentes = 2;
+
+    System.out.println((salarioMensal < mediaSalario) && (quantidadeDependentes >= mediaDependentes));
+
+    boolean salarioBaixo = salarioMensal < mediaSalario;
+    boolean muitosDependentes = quantidadeDependentes >= mediaDependentes;
+
+    System.out.println((salarioBaixo) && (muitosDependentes));
+
+    boolean recebeAuxilio = (salarioBaixo) && (muitosDependentes);
+
+    System.out.println("Recebe o auxilio " + recebeAuxilio);
+    }
+}
