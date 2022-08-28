afd986c3fefc59d1e7103eff3090767f90faa32e
Renato Shigueo Fujimoto <98925481+Renatofujimoto@users.noreply.github.com>
2022-08-28 20:47:29 -0300

    Update Main.java

--- Java/operadores-logicos/Main.java
+++ Java/operadores-logicos/Main.java
@@ -1,21 +1,38 @@
 package operadoresLogicos;
 
+import java.util.Scanner;
+
 // programa para verificar quanto o funcionario deve receber de acordo com o tempo da empresa e horas trabalhadas
+
 public class Main {
     public static void main(String[] args){
-            int quantidadeAnos = 5;
-            int horasTrabalhadas = 40;
-            int valorHora = 50;
-            int salario = 0;
+      Scanner scan =  new Scanner(System.in);
+      int a,b,c;
 
-            if (quantidadeAnos <= 1) {
-                salario = 1500 + (valorHora * horasTrabalhadas);
-            } else if ((quantidadeAnos > 1) && (quantidadeAnos < 3)) {
-                salario = 2000 + (valorHora * horasTrabalhadas);
-            } else {
-                salario = 3000 + (valorHora * horasTrabalhadas);
-            }
+      System.out.println("Digite a quantidade de anos: ");
+      a = scan.nextInt();
 
-            System.out.println("salario deve ser: " + salario);
-        }
+      System.out.println("Digite a quantidade de horas: ");
+      b = scan.nextInt();
+
+      System.out.println("Digite o valor das horas: ");
+      c = scan.nextInt();
+
+
+      int quantidadeAnos = a;
+      int horasTrabalhadas = b;
+      int valorHora = c;
+      int salario = 0;
+
+
+      if (quantidadeAnos <= 1) {
+      salario = 1500 + (valorHora * horasTrabalhadas);
+      } else if ((quantidadeAnos > 1) && (quantidadeAnos < 3)) {
+      salario = 2000 + (valorHora * horasTrabalhadas);
+      } else {
+      salario = 3000 + (valorHora * horasTrabalhadas);
+      }
+
+      System.out.println("salario deve ser: " + salario);
+   }
 }
